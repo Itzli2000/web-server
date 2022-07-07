4c37c940cb4622c521e2e38e1ce9eaed534c6c94
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2022-07-07 17:16:55 -0500

    Created basic server structure

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("first");
+const http = require("http");
+const port = 8080;
+
+http
+  .createServer((req, res) => {
+    res.write("hello world");
+    res.end();
+  })
+  .listen(port);
+
+console.log("Listening in port ", port);
