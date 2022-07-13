8db02e4a94139cfa02c481f8f87eef49401bfd2c
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2022-07-12 20:47:19 -0500

    ompleted v1 of application

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const hbs = require("hbs");
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials", function (err) {});
