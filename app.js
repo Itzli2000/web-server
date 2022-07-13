a7259f903e0ba8e1781ad8ef75c991dc45b04396
Ariel Itzli Molina Sánchez <itzli2000@msn.com>
2022-07-12 19:25:58 -0500

    Added handlebar and created specific views

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 const port = 8080;
 
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+
 // Serve static content
 app.use(express.static(`${__dirname}/public`));
 
+app.get("/", function (req, res) {
+  res.render("home", {
+    name: "Ariel Molina",
+    title: "Node course",
+  });
+});
+
 app.get("/generic", function (req, res) {
-    res.sendFile(`${__dirname}/public/generic.html`);
+  // res.sendFile(`${__dirname}/public/generic.html`);
+  res.render("generic", {
+    name: "Ariel Molina",
+    title: "Node course",
+  });
 });
 
 app.get("/elements", function (req, res) {
-    res.sendFile(`${__dirname}/public/elements.html`);
+  // res.sendFile(`${__dirname}/public/elements.html`);
+  res.render("elements", {
+    name: "Ariel Molina",
+    title: "Node course",
+  });
 });
 
 app.get("/test", function (req, res) {
